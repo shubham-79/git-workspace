1fb14cef92f6156ccc7b9304a5a65a657b2005f5
shubham Tilekar <tilekar79s@gmail.com>
2022-07-31 20:18:44 +0530

    first commit

---
+++ Ascii_value.java
@@ -0,0 +1,11 @@
+public class Ascii_value {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        
+       
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        }
+}
